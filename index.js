574cd20987a15f85d235891acbbe5014a15ca926
Lynn Kim <lynn.kim@brandung.de>
2022-12-16 10:20:45 +0100

    Separation of Display and Calculation values

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const display = document.querySelector(".display");
 const displayOperation = display.querySelector(".display__operation");
+const displayPretty = display.querySelector(".display__pretty");
 const displayResult = display.querySelector(".display__result");
-// let operation = {
-//         displayValue: "",
-//         calculation: calculation += dataset.buttonOperation
-// };
+
 let operation = "";
+let pretty = "";
 let result = "";
 
 //Calculator buttonHover, click
@@ -25,64 +24,71 @@ buttons.forEach(button => {
 
 //button click
 function buttonPress(event){
-        // let coma = operation.toLocalString("de-DE");
         if (!event.target.matches("button")){
                 return;
         }
         if(event.target.dataset.buttonRemove === "AC"){
                 operation = '';
+                pretty = '';
                 result = '';
                 return;
         }
         if(event.target.dataset.buttonRemove === "DEL"){
                 operation = operation.substr(0, operation.length-1);
+                pretty = pretty.substr(0, pretty.length-1);
                 result = '';
                 return;
         }
-        // if(event.target.classList.contains("multiply")){
-        //         operation += '*';
-        //         // multiple = multiple.replace('x', '*');
-        //         // operation += multiple;
-        //         console.log(operation);
-        //         return;
-        // }
-        // if(event.target.classList.contains("division")){
-        //         operation += '/';
-        //         console.log(operation);
-        //         return;
-        // }
-        // if(event.target.classList.contains("plus")){
-        //         operation += '+';
-        //         console.log(operation);
-        //         return;
-        // }
-        // if(event.target.classList.contains("minus")){
-        //         operation += '-';
-        //         console.log(operation);
-        //         return;
-        // }
-        // if(event.target.classList.contains("decimal")){
-        //         operation += '.';
-        //         console.log(operation);
-        //         return;
-        // }
-        // if(event.target.classList.contains("percent")){
-        //         operation += '*0.01';
-        //         alert("How to use the % calculator: Multiply the numbers first. Then add a % to the last number.")
-        //         console.log(operation);
-        //         return;
-        // }
+        if(event.target.dataset.buttonOperation === "*"){
+                operation += "*";
+                pretty += "x";
+                console.log(operation);
+                return;
+        }
+        if(event.target.dataset.buttonOperation === "/"){
+                operation += '/';
+                pretty += "÷";
+                console.log(operation);
+                return;
+        }
+        if(event.target.dataset.buttonOperation === "+"){
+                operation += '+';
+                pretty += "+";
+                console.log(operation);
+                return;
+        }
+        if(event.target.dataset.buttonOperation === "-"){
+                operation += '-';
+                pretty += "-";
+                console.log(operation);
+                return;
+        }
+        if(event.target.dataset.buttonOperation === "."){
+                operation += '.';
+                pretty += ",";
+                console.log(operation);
+                return;
+        }
+        if(event.target.dataset.buttonOperation === "*0.01"){
+                operation += '*0.01';
+                pretty += "%";
+                alert("How to use the % calculator: Multiply the numbers first. Then add a % to the last number.")
+                console.log(operation);
+                return;
+        }
 
         if(event.target.dataset.buttonOperation === "result"){
                 operation = new Function(`return ${operation}`)(result);
                 result = operation;
+                console.log(operation);
                 return;
         }
         operation += event.target.dataset.buttonOperation;
+        pretty += event.target.dataset.buttonOperation;
         console.log(operation);
 }
 // display update-show window
 function updateDisplay(){
-        displayOperation.innerText= operation;
+        displayPretty.innerText = pretty;
         displayResult.innerText= result;
 }
