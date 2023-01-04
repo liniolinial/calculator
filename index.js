fb9a97b75232b8005486342c3bd54e7bf1903eee
Lynn Kim <lynn.kim@brandung.de>
2023-01-04 12:58:27 +0100

    Try1. keypress

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const displayOperation = display.querySelector(".display__operation");
 const displayScreen = display.querySelector(".display__screen");
 const displayResult = display.querySelector(".display__result");
 
-
 let operation = "";
 let screen = ""; 
 let result = "";
@@ -14,19 +13,78 @@ console.log("buttons", buttons);
 buttons.forEach(button => {
         button.addEventListener("click", function(event) {
                 buttonPress(event);
-                // stringNumberToInt(screen)
                 updateDisplay();
         });
+        document.addEventListener("keydown", function(event) {
+                keyPress(event.key);
+                updateDisplay();
+                // keyPressHover(event.key);
+        });
 })
-//keyboard Press-> noch nicht
-// document.addEventListener("keydown", function(ev){
-//         buttonAnimation(ev.target);
-//     });
+
+// key press value
+function keyPress(key){
+        switch(key) {
+                case "1":  
+                        operation = "1";
+                        screen = "1";
+                        console.log(operation);
+                        break;
+
+                case "3":
+                        operation = "3";
+                        screen = "3";
+                        console.log(operation);
+                        break;
+                
+                case "Enter":
+                        // var key = new Audio("sound/crash.mp3")
+                        // key.play();
+                        operation = new Function(`return ${operation}`)(result);
+                        result = operation;
+                        console.log(operation);
+                        break;
+
+                default: console.log(operation)
+                }
+                
+  }
+
+//keypress animation css
+// function keyPressHover(currentKey){
+//         var activeButton = document.querySelectorAll("." + currentKey);
+//         activeButton.classList.add("calc-btn");
+//         setTimeout(function(){
+//             activeButton.classList.remove("calc-btn");
+//         }, 100);
+//     }
+
+// document.addEventListener("keypress", function (ev) {
+// function keyPress(ev){
+//         // decimal point
+//         if ([194].includes(ev.charCode)) {
+//                 // , .
+//                 operation += '.';
+//                 screen += ",";
+//                 input(".");
+//                 return;
+//                 console.log(operation);
+//         }
+//         // digits
+//         if ([48, 49, 50, 51, 52, 53, 54, 55, 56, 57].includes(ev.charCode)) {
+//                 // 0-9
+//                 input(ev.charCode - 48);
+//         }
+//         operation += ev.target.dataset.buttonOperation;
+//         screen += ev.target.dataset.buttonOperation;
+//         console.log(operation);
+// }
+
+
 
 //button click
 function buttonPress(event){
         if (!event.target.matches("button")){
-
                 return;
         }
         if(event.target.dataset.buttonRemove === "AC"){
@@ -43,7 +101,6 @@ function buttonPress(event){
         }
         if(event.target.dataset.buttonOperation === "*"){
                 operation += "*";
-                // screen = parseInt(screen).toLocaleString("de-DE");
                 screen += "x";
                 console.log(operation);
                 return;
@@ -75,18 +132,18 @@ function buttonPress(event){
         if(event.target.dataset.buttonOperation === "*0.01"){
                 operation += '*0.01';
                 screen += "%";
-                alert("How to use the % calculator: Multiply the numbers first. Then add a % to the last number.")
                 console.log(operation);
                 return;
         }
         if(event.target.dataset.buttonOperation === "result"){
                 operation = new Function(`return ${operation}`)(result);
                 result = operation;
-                // screen += "=";
                 console.log(operation);
                 return;
         }
-        // frühere result if kondition weg und probiere mit einzelen separaten operation funktion ohne new funktion.
+        // frühere result if kondition weg und probiere mit einzelen -
+        // separaten operation funktion ohne new funktion.
+        
         // if(event.target.dataset.buttonOperation === "result"){
         //         //zahl operator zahl2 = result
         //         //welcher operator gedruckt wurde
@@ -96,20 +153,17 @@ function buttonPress(event){
         console.log(operation);
 }
 
-//parseInt helper function
-// function getNumber({ value, defaultValue }) {
-//         const num = parseInt(value, 10);
-//         return isNaN(num) ? defaultValue : num;
-//       }
+//numberSize
+// function numberSize(){
+//         if (event.target.matches.getElementsByClassName("calc-btn", "operator"))
+//         operation = operation.substr(0, operation.length-1);
+//         screen = screen.substr(0, screen.length-1);
+// }
 
-// display update-show window
+// display update-show window + NumberSize
 function updateDisplay(){
-        //number/string
-
-        // console.log(typeof screen);
-        // console.log(parseInt(screen));
         // displayOperation.innerText = operation;
-        displayScreen.innerText = screen;  //parseInt(screen,10)
+        displayScreen.innerText = screen;
         console.log(typeof result);
-        displayResult.innerText= result.toLocaleString("de-DE");   //parseInt(result,10)
+        displayResult.innerText= result.toLocaleString("de-DE");
 }
